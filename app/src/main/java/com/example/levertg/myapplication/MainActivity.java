ddf5bf218de675ae5aaeb8458d774ff929b588d4
Gautier Levert <gautierlevert@gmail.com>
2017-03-29 22:51:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/levertg/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.levertg.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW)
+                        .addCategory(Intent.CATEGORY_BROWSABLE)
+                        .setData(Uri.parse("http://google.com"));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/levertg/myapplication/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.levertg.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
